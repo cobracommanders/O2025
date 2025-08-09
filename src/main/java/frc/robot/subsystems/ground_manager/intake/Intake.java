544d6a8f67493d8a27dc199a2b7e320b3f088cb6
Quinten Arnone <quintenarnone@gmail.com>
2025-08-09 12:54:10 -0700

    Update Intake.java

--- src/main/java/frc/robot/subsystems/ground_manager/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/ground_manager/intake/Intake.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems.ground_manager.intake;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import frc.robot.stateMachine.StateMachine;
+
 public class Intake extends StateMachine {
-    public TalonFX = new TalonFX();
 }
