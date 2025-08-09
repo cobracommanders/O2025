172d19a8ceaabee3850fa52c34480bb9ac744488
GavinDistel <69681028+Gryffindor528@users.noreply.github.com>
2025-08-09 12:03:21 -0700

    added arm constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,13 @@
 package frc.robot;
 
 public class Constants {
-    
+    public static final class ArmConstants {
+        public static final double P = 0;
+        public static final double I = 0;
+        public static final double D = 0;
+        public static final double G = 0;
+        public static final double MotionMagicCruiseVelocity = 0;
+        public static final double MotionMagicAcceleration = 0;
+        public static final double MotionMagicJerk = 0;
+    }
 }
