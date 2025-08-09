a1232b7e9bd22946185c7166bbfaf12a6c6e74a0
GavinDistel <69681028+Gryffindor528@users.noreply.github.com>
2025-08-09 13:49:28 -0700

    Fix typos in arm position and state names

--- src/main/java/frc/robot/subsystems/armManager/arm/ArmPositions.java
+++ src/main/java/frc/robot/subsystems/armManager/arm/ArmPositions.java
@@ -2,9 +2,9 @@ package frc.robot.subsystems.armManager.arm;
 
 public class ArmPositions {
     public static final double IDLE = 0;
-    public static final double INTAKE_GROUND_ALAGAE = 0;
+    public static final double INTAKE_GROUND_ALGAE = 0;
     public static final double INTAKE_HIGH_REEF_ALGAE = 0;
     public static final double INTAKE_LOW_REEF_ALGAE = 0;
-    public static final double ALAGAE_PROCESSOR = 0;
-    public static final double ALAGAE_NET = 0;
+    public static final double ALGAE_PROCESSOR = 0;
+    public static final double ALGAE_NET = 0;
 }
