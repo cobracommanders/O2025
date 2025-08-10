a29ba811a75c3449949903978379616c9fad0fb2
GavinDistel <69681028+Gryffindor528@users.noreply.github.com>
2025-08-09 20:23:28 -0700

    Generated Swerve Code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,4 +11,9 @@ public class Constants {
         public static final double MotionMagicJerk = 0;
         public static final double encoderOffset = 0;
     }
+
+    public static final class DrivertrainConstants {
+        public static final double MAX_VELOCITY_METERS_PER_SECOND = 5.94;
+        public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 300;
+    }
 }
