6f9ad2cc3b85a4217aab212fcb0a1e22c9bd756e
GavinDistel <69681028+Gryffindor528@users.noreply.github.com>
2025-08-09 20:40:01 -0700

    cleanup

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,18 +1,6 @@
 package frc.robot;
 
 public class Constants {
-    
-    public static final class ArmConstants {
-        public static final double P = 0;
-        public static final double I = 0;
-        public static final double D = 0;
-        public static final double G = 0;
-        public static final double MotionMagicCruiseVelocity = 0;
-        public static final double MotionMagicAcceleration = 0;
-        public static final double MotionMagicJerk = 0;
-        public static final double encoderOffset = 0;
-    }
-
     public static final class DrivertrainConstants {
         public static final double MAX_VELOCITY_METERS_PER_SECOND = 5.94;
         public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 300;
