958339c965809a3ef0ab9786fb85335d2e217e7e
GavinDistel <69681028+Gryffindor528@users.noreply.github.com>
2025-08-12 18:44:39 -0700

    added Hand motor ports and constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,52 +1,52 @@
 package frc.robot;
 
 public final class Constants {
-  public static final class ArmConstants {
-      public static final double P = 0;
-      public static final double I = 0;
-      public static final double D = 0;
-      public static final double G = 0;
-      public static final double MotionMagicCruiseVelocity = 0;
-      public static final double MotionMagicAcceleration = 0;
-      public static final double MotionMagicJerk = 0;
-      public static final double encoderOffset = 0;
-  }
+    public static final class ArmConstants {
+        public static final double P = 0;
+        public static final double I = 0;
+        public static final double D = 0;
+        public static final double G = 0;
+        public static final double MotionMagicCruiseVelocity = 0;
+        public static final double MotionMagicAcceleration = 0;
+        public static final double MotionMagicJerk = 0;
+        public static final double encoderOffset = 0;
+    }
 
-  public static final class IntakePivotConstants{
-    public static final double positionIncrement = 0;
-    public static final double P = 0.0;
-    public static final double I = 0.0;
-    public static final double D = 0.0;
-    public static final double G = 0.0;
-    public static final double MotionMagicAcceleration = 0.0;
-    public static final double MotionMagicCruiseVelocity = 0.0;
-    public static final double MotionMagicJerk = 0.0;
-  }
+    public static final class IntakePivotConstants {
+        public static final double positionIncrement = 0;
+        public static final double P = 0.0;
+        public static final double I = 0.0;
+        public static final double D = 0.0;
+        public static final double G = 0.0;
+        public static final double MotionMagicAcceleration = 0.0;
+        public static final double MotionMagicCruiseVelocity = 0.0;
+        public static final double MotionMagicJerk = 0.0;
+    }
 
-  public static final class IntakeRollersConstants{
-    public static final double stallCurrent = 0.0;
-  }
-  
-  public static final class DrivertrainConstants {
-    public static final double MAX_VELOCITY_METERS_PER_SECOND = 5.94;
-    public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 300;
-  }
+    public static final class IntakeRollersConstants {
+        public static final double stallCurrent = 0.0;
+    }
 
-  public static class ClimberConstants{
-    //TODO: Find the values for these.
-    public static final double DEPLOY_MOTION_MAGIC_CRUISE_VELOCITY = 0;
-    public static final double DEPLOY_MOTION_MAGIC_ACCELERATION = 0;
-    public static final double DEPLOY_MOTION_MAGIC_JERK = 0;
-    public static final double CAGE_DETECECTION_CURRENT = 0;
-  }
+    public static final class DrivertrainConstants {
+        public static final double MAX_VELOCITY_METERS_PER_SECOND = 5.94;
+        public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 300;
+    }
 
-  public static final class HandConstants {
-    public static final double coralStallCurrent = 0;
-    public static final double algaeStallCurrent = 0;
-  }
+    public static class ClimberConstants {
+        // TODO: Find the values for these.
+        public static final double DEPLOY_MOTION_MAGIC_CRUISE_VELOCITY = 0;
+        public static final double DEPLOY_MOTION_MAGIC_ACCELERATION = 0;
+        public static final double DEPLOY_MOTION_MAGIC_JERK = 0;
+        public static final double CAGE_DETECECTION_CURRENT = 0;
+    }
 
-  public static final class OIConstants {
-    public static final int DRIVER_CONTROLLER_ID = 0;
-    public static final int OPERATOR_CONTROLLER_ID = 1;
-}
+    public static final class HandConstants {
+        public static final double coralStallCurrent = 0;
+        public static final double algaeStallCurrent = 0;
+    }
+
+    public static final class OIConstants {
+        public static final int DRIVER_CONTROLLER_ID = 0;
+        public static final int OPERATOR_CONTROLLER_ID = 1;
+    }
 }
--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -18,7 +18,6 @@ public class Ports {
         public static int WHEEL_CLIMBER_MOTOR_PORT = 0;
         public static int WINCH_CLIMBER_MOTOR_PORT = 0;
     }
-    
     public static class ArmPorts {
         public static final int MOTOR = 0; //22
         public static final int ENCODER = 0; //0
