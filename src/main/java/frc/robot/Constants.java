15bbf23dd56f794a40643efed7ab17fca7a311fd
GavinDistel <69681028+Gryffindor528@users.noreply.github.com>
2025-08-12 18:44:39 -0700

    added Hand motor ports and constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,19 +27,32 @@ public final class Constants {
     public static final double stallCurrent = 0.0;
   }
   public static final class DrivertrainConstants {
-      public static final double MAX_VELOCITY_METERS_PER_SECOND = 5.94;
-      public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 300;
+    public static final double MAX_VELOCITY_METERS_PER_SECOND = 5.94;
+    public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 300;
   }
   public static class ClimberConstants{
-      //TODO: Find the values for these.
-      public static final double DEPLOY_MOTION_MAGIC_CRUISE_VELOCITY = 0;
-      public static final double DEPLOY_MOTION_MAGIC_ACCELERATION = 0;
-      public static final double DEPLOY_MOTION_MAGIC_JERK = 0;
-      public static final double CAGE_DETECECTION_CURRENT = 0;
+    //TODO: Find the values for these.
+    public static final double DEPLOY_MOTION_MAGIC_CRUISE_VELOCITY = 0;
+    public static final double DEPLOY_MOTION_MAGIC_ACCELERATION = 0;
+    public static final double DEPLOY_MOTION_MAGIC_JERK = 0;
+    public static final double CAGE_DETECECTION_CURRENT = 0;
   }
   public static final class HandConstants {
     public static final double coralStallCurrent = 0;
     public static final double algaeStallCurrent = 0;
   }
+  public static final class DrivertrainConstants {
+    public static final double MAX_VELOCITY_METERS_PER_SECOND = 5.94;
+    public static final double MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 300;
+  }
+  public static class ClimberConstants{
+    //TODO: Find the values for these.
+    public static final double DEPLOY_MOTION_MAGIC_CRUISE_VELOCITY = 0;
+    public static final double DEPLOY_MOTION_MAGIC_ACCELERATION = 0;
+    public static final double DEPLOY_MOTION_MAGIC_JERK = 0;
+    public static final double CAGE_DETECECTION_CURRENT = 0;
+  }
+  public static final class HandConstants {
+      public static final double coralStallCurrent = 0;
+  }
 }
-
