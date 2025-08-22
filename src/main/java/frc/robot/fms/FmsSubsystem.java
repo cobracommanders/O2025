319cc3a29e004c51f1ce3a911457a302e2fc767d
GavinDistel <gdistel@498robotics.org>
2025-08-21 19:40:58 -0700

    added checks for driverstation enabled

--- src/main/java/frc/robot/fms/FmsSubsystem.java
+++ src/main/java/frc/robot/fms/FmsSubsystem.java
@@ -7,12 +7,36 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.drivetrain.DriveSubsystem;
 
 public class FmsSubsystem extends SubsystemBase {
+    private boolean disabled= true;
+    private boolean teleopEnabled = false;
+    private boolean autoEnabled = false;
+    private Alliance alliance = Alliance.Red;
 
     public FmsSubsystem() {
     }
 
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    public boolean isTeleop() {
+        return teleopEnabled;
+    }
+
+    public boolean isAutonomous() {
+        return autoEnabled;
+    }
+
+    public boolean isRedAlliance() {
+        return alliance == Alliance.Red;
+    }
+
     @Override
     public void periodic() {
+        disabled = DriverStation.isDisabled();
+        teleopEnabled = DriverStation.isTeleop();
+        autoEnabled = DriverStation.isAutonomous();
+        alliance = DriverStation.getAlliance().orElse(Alliance.Red);
         DogLog.log("Fms/Alliance", isRedAlliance() ? "Red" : "Blue");
     }
 
