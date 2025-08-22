c6e77cf1cc15d889471f4a1173e7195728409661
GavinDistel <gdistel@498robotics.org>
2025-08-21 19:37:22 -0700

    renamed robotManager in commands file

--- src/main/java/frc/robot/commands/RobotCommands.java
+++ src/main/java/frc/robot/commands/RobotCommands.java
@@ -10,37 +10,77 @@ import frc.robot.stateMachine.RobotState;
 
 public class RobotCommands {
 
-    private final RobotManager robot;
+    private final RobotManager robotManager;
     private final Subsystem[] requirements;
 
     public RobotCommands(){
-        this.robot = RobotManager.getInstance();
-        var requirementsList = List.of(robot.armManager, robot.groundManager, robot.climber);
+        this.robotManager = RobotManager.getInstance();
+        var requirementsList = List.of(robotManager.armManager, robotManager.groundManager, robotManager.climber);
         requirements = requirementsList.toArray(Subsystem[]::new);
     }
 
     public Command scoreCommand() {
-        return Commands.runOnce(robot::scoreRequest, requirements)
-                .andThen(Commands.waitUntil(() -> robot.getState() == RobotState.IDLE));
+        return Commands.runOnce(robotManager::scoreRequest, requirements)
+                .andThen(Commands.waitUntil(() -> robotManager.getState() == RobotState.IDLE)).withName("score");
     }
 
     public Command algaeIntakeCommand() {
-        return Commands.runOnce(robot::intakeAlgaeRequest, requirements)
-                .andThen(Commands.waitUntil(() -> robot.getState() == RobotState.IDLE));
+        return Commands.runOnce(robotManager::intakeAlgaeRequest, requirements)
+                .andThen(Commands.waitUntil(() -> robotManager.getState() == RobotState.IDLE)).withName("algaeIntake");
     }
 
     public Command coralIntakeCommand() {
-        return Commands.runOnce(robot::coralIntakeRequest, requirements)
-                .andThen(Commands.waitUntil(() -> robot.getState() == RobotState.IDLE));
+        return Commands.runOnce(robotManager::coralIntakeRequest, requirements)
+                .andThen(Commands.waitUntil(() -> robotManager.getState() == RobotState.IDLE)).withName("coralIntake");
     }
 
-    public Command scoreLevelCommand() {
-        return Commands.runOnce(robot::scoreLevelRequest, requirements);
+    public Command prepareScoreCommand() {
+        return Commands.runOnce(robotManager::scoreLevelRequest, requirements).withName("prepareScore");
                 //.andThen(Commands.waitUntil(() -> robot.getState() == RobotState.WAIT_L1 || robot.getState() == RobotState.WAIT_L4));
     }
 
     public Command climbCommand() {
-        return Commands.runOnce(robot::climbRequest, requirements)
-                .andThen(Commands.waitUntil(() -> robot.getState() == RobotState.CLIMB));
+        return Commands.runOnce(robotManager::climbRequest, requirements)
+                .andThen(Commands.waitUntil(() -> robotManager.getState() == RobotState.CLIMB)).withName("climb");
+    }
+
+    public Command setProcessorCommand(){
+        return Commands.runOnce(() -> robotManager.setProcessor()).withName("setProcessor");
+    }
+
+    public Command setBargeCommand(){
+        return Commands.runOnce(() -> robotManager.setBarge()).withName("setBarge");
+    }
+
+    public Command setL1Command(){
+        return Commands.runOnce(() -> robotManager.setL1()).withName("setL1");
+    }
+
+    public Command setL2Command(){
+        return Commands.runOnce(() -> robotManager.setL2()).withName("setL2");
+    }
+
+    public Command setL3Command(){
+        return Commands.runOnce(() -> robotManager.setL3()).withName("setL3");
+    }
+
+    public Command setL4Command(){
+        return Commands.runOnce(() -> robotManager.setL4()).withName("setL4");
+    }
+
+    public Command setHighReefAlgaeCommand(){
+        return Commands.runOnce(() -> robotManager.setHighReefAlgae()).withName("setHighReefAlgae");
+    }
+
+    public Command setLowReefAlgaeCommand(){
+        return Commands.runOnce(() -> robotManager.setLowReefAlgae()).withName("setLowReefAlgae");
+    }
+
+    public Command setGroundAlgaeCommand(){
+        return Commands.runOnce(() -> robotManager.setGroundAlgae()).withName("setGroundAlgae");
+    }
+
+    public Command resetToIdleCommand(){
+        return Commands.runOnce(() -> robotManager.resetToIdleRequest()).withName("resetToIdle");
     }
 }
