37dc68a49c7d3af0d5f50bea5d7ac0076144beb5
Quinten Arnone <quintenarnone@gmail.com>
2025-08-23 14:59:39 -0700

    made l4 auto

--- src/main/java/frc/robot/subsystems/armManager/ArmManager.java
+++ src/main/java/frc/robot/subsystems/armManager/ArmManager.java
@@ -40,11 +40,11 @@ public class ArmManager extends StateMachine<ArmManagerStates> {
                     nextState = ArmManagerStates.IDLE;
                 }
             }
-            case PREPARE_SCORE_L4 -> {
-                if (armScheduler.isReady()) {
-                    nextState = ArmManagerStates.WAIT_L4;
-                }
-            }
+            // case PREPARE_SCORE_L4 -> {
+            //     if (armScheduler.isReady()) {
+            //         nextState = ArmManagerStates.WAIT_L4;
+            //     }
+            // }
 
             case SCORE_L4 -> {
                 if(timeout(scoringTime)){
