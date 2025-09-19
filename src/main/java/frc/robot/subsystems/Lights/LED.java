93b998c65435ea16fb460a38f04f2b2dac099582
gatlinfarrington <gatlinf@utexas.edu>
2025-09-19 08:19:51 -0700

    cleanup, intake leds

--- src/main/java/frc/robot/subsystems/Lights/LED.java
+++ src/main/java/frc/robot/subsystems/Lights/LED.java
@@ -4,36 +4,29 @@
 
 package frc.robot.subsystems.Lights;
 
-
-import java.security.cert.LDAPCertStoreParameters;
-
-import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.AddressableLEDBufferView;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.config.FeatureFlags;
 import frc.robot.stateMachine.OperatorOptions;
 import frc.robot.stateMachine.OperatorOptions.CoralMode;
 import frc.robot.stateMachine.RequestManager;
 import frc.robot.subsystems.climber.ClimberStates;
-import frc.robot.subsystems.ground_manager.coraldetection.CoralDetectorStates;
+import frc.robot.subsystems.ground_manager.GroundManager;
+import frc.robot.subsystems.ground_manager.coraldetection.CoralDetector;
 
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.*;
 
 public class LED {
     private final AddressableLED glowjack_horseman;
     private final AddressableLEDBuffer m_ledBuffer;
     private final AddressableLEDBufferView m_middle;
 
-    // Rainbow LED pattern
-    public final LEDPattern m_pattern = LEDPattern.rainbow(255, 128);
-    private static final Distance kLedSpacing = Meters.of(1 / 120.0);
-    private final LEDPattern m_scrollingRainbow =
-            m_pattern.scrollAtAbsoluteSpeed(MetersPerSecond.of(1), kLedSpacing);
+    boolean isBlinking;
+
+    boolean tryCoralBlink = true;
 
     public LED() {
         // PWM port 9
@@ -48,51 +41,66 @@ public class LED {
         // Set the data
         glowjack_horseman.setData(m_ledBuffer);
         glowjack_horseman.start();
+
+        isBlinking = false;
     }
     Color c;
     public void periodic() {
-        switch(OperatorOptions.getInstance().scoreLocation){
-            case L1:
-                c = Color.kRed;
-                LEDPattern.solid(c).applyTo(m_ledBuffer);
-                break;
-            case L2:
-                c = Color.kYellow;
-                LEDPattern.solid(c).applyTo(m_ledBuffer);
-                break;
-            case L3:
-                c = Color.kDodgerBlue;
-                LEDPattern.solid(c).applyTo(m_ledBuffer);
-                break;
-            case L4:
-                c = Color.kGreen;
-                LEDPattern.solid(c).applyTo(m_ledBuffer);
-                break;
-            case PROCESSOR:
-                c = Color.kDarkOliveGreen;
-                LEDPattern.solid(c).applyTo(m_ledBuffer);
-                break;
-            case BARGE:
-                c = Color.kPurple;
-                LEDPattern.solid(c).applyTo(m_ledBuffer);
-                break;
-            default:
-                c = Color.kBlack;
-                LEDPattern.solid(c).applyTo(m_ledBuffer);
-                break;
+        //when the robot is blinking, the cage has been detected, robot is done for the match
+        if(!isBlinking) {
+            switch (OperatorOptions.getInstance().scoreLocation) {
+                case L1:
+                    c = Color.kRed;
+                    LEDPattern.solid(c).applyTo(m_ledBuffer);
+                    break;
+                case L2:
+                    c = Color.kYellow;
+                    LEDPattern.solid(c).applyTo(m_ledBuffer);
+                    break;
+                case L3:
+                    c = Color.kDodgerBlue;
+                    LEDPattern.solid(c).applyTo(m_ledBuffer);
+                    break;
+                case L4:
+                    c = Color.kGreen;
+                    LEDPattern.solid(c).applyTo(m_ledBuffer);
+                    break;
+                case PROCESSOR:
+                    c = Color.kDarkOliveGreen;
+                    LEDPattern.solid(c).applyTo(m_ledBuffer);
+                    break;
+                case BARGE:
+                    c = Color.kPurple;
+                    LEDPattern.solid(c).applyTo(m_ledBuffer);
+                    break;
+                default:
+                    c = Color.kBlack;
+                    LEDPattern.solid(c).applyTo(m_ledBuffer);
+                    break;
+            }
+
+            if (OperatorOptions.getInstance().coralMode == CoralMode.CORAL_MODE) {
+                LEDPattern.solid(Color.kWhite).applyTo(m_middle);
+            } else {
+                LEDPattern.solid(c).applyTo(m_middle);
+            }
+
+            //start blinking the LEDs .5 seconds before the climber starts pulling the robot up
+            if (RequestManager.getInstance().climber.getState() == ClimberStates.CONTINUE_SUCKING) {
+                LEDPattern.solid(c).blink(Seconds.of(.2)).applyTo(m_ledBuffer);
+                isBlinking = true;
+            }
+
+            //Can we periodically assign the blink pattern? Or does it need to only be assigned once?
+            if(FeatureFlags.LED_INTAKE_BLINK.getAsBoolean()){
+                if(CoralDetector.getInstance().hasCoral()){
+                    LEDPattern.solid(c).blink(Seconds.of(.2)).applyTo(m_ledBuffer);
+                }
+
+            }
+
+            glowjack_horseman.setData(m_ledBuffer);
         }
-
-        if(OperatorOptions.getInstance().coralMode == CoralMode.CORAL_MODE){
-            LEDPattern.solid(Color.kWhite).applyTo(m_middle);
-        }else{
-            LEDPattern.solid(c).applyTo(m_middle);
-        }
-
-        if(RequestManager.getInstance().climber.getState() != ClimberStates.IDLE){
-
-        }
-
-        glowjack_horseman.setData(m_ledBuffer);
 
     }
 }
