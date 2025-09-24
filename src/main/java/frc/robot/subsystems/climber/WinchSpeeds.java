849a35de6c92f27544c06bb9f8bcb7787ae48183
Quinten Arnone <quintenarnone@gmail.com>
2025-09-23 17:51:38 -0700

    Set deploying speed slower

--- src/main/java/frc/robot/subsystems/climber/WinchSpeeds.java
+++ src/main/java/frc/robot/subsystems/climber/WinchSpeeds.java
@@ -3,6 +3,6 @@ package frc.robot.subsystems.climber;
 public class WinchSpeeds {
     public static final double IDLE = 0;
     public static final double CLIMBING = -1.0;
-    public static final double DEPLOYING = -1.0;
+    public static final double DEPLOYING = -0.5;
 }
     
