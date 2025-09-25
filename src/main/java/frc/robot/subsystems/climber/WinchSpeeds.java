fe01c623bed1d90226dd72a4df409077e8c54c0d
GavinDistel <gdistel@498robotics.org>
2025-09-24 20:35:29 -0700

    Reapply "Fixed Climber winch test code"
    This reverts commit 373b18363f9543b2f00aff531e5fa20e327d6844.

--- src/main/java/frc/robot/subsystems/climber/WinchSpeeds.java
+++ src/main/java/frc/robot/subsystems/climber/WinchSpeeds.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.climber;
 public class WinchSpeeds {
     public static final double IDLE = 0;
     public static final double CLIMBING = -1.0;
-    public static final double DEPLOYING = -0.5;
+    public static final double DEPLOYING = -1.0;
+    public static final double TEST_SPEEDS = 0.5;
 }
     
