a26664c06147ebecdb0eb6286d92acc6200adbaa
gatlinfarrington <gatlinf@utexas.edu>
2025-10-15 22:40:21 -0700

    increase intake speed

--- src/main/java/frc/robot/subsystems/ground_manager/intakeRollers/IntakeRollersSpeeds.java
+++ src/main/java/frc/robot/subsystems/ground_manager/intakeRollers/IntakeRollersSpeeds.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.ground_manager.intakeRollers;
 
 public class IntakeRollersSpeeds {
     public static final double IDLE = 0.0;
-    public static final double INTAKING = -0.5;
+    public static final double INTAKING = -1;
     public static final double HANDOFF = 1.0;
     public static final double SCORE_L1 = 0.5;
     public static final double CLIMB = 0.0;
