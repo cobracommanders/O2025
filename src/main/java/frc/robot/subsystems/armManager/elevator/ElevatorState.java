d157c2bf79d1e29e803195f8930950ef2169c7a8
Nolan Locke <nolan.s.locke@gmail.com>
2025-10-16 22:21:43 -0700

    lower elevator position during handoff

--- src/main/java/frc/robot/subsystems/armManager/elevator/ElevatorState.java
+++ src/main/java/frc/robot/subsystems/armManager/elevator/ElevatorState.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.armManager.elevator;
 
 import dev.doglog.DogLog;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.DoubleSubscriber;
 
 public enum ElevatorState {
@@ -11,7 +12,7 @@ public enum ElevatorState {
     GROUND_ALGAE(0),
     ALGAE_NET(1.38),
     ALGAE_PROCESSOR(0.1),
-    HANDOFF(0.775), // Actual handoff position
+    HANDOFF(0.775 - Units.inchesToMeters(0.75)), // Actual handoff position
     IDLE_EMPTY(0.825), // State above the intake that allows the intake to move in/out freely
     LOLLIPOP(0),
     PREPARE_L4(1.3),
