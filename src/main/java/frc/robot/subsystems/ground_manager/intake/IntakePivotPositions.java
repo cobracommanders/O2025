207693e27354ad8ad9f49088f3791fa29dc5bc73
Nolan Locke <nolan.s.locke@gmail.com>
2025-10-16 22:27:06 -0700

    match IDLE and HANDOFF intake pivot positions

--- src/main/java/frc/robot/subsystems/ground_manager/intake/IntakePivotPositions.java
+++ src/main/java/frc/robot/subsystems/ground_manager/intake/IntakePivotPositions.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems.ground_manager.intake;
 public class IntakePivotPositions {
-    public static double IDLE = 0.3;
+    public static double IDLE = 0.318;
     public static double INTAKING = -0.036;
     public static double HANDOFF = 0.318;
     public static double SCORE_L1 = 0.25;
