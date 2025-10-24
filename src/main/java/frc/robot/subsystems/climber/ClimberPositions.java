451db1200ba7d4da266f66de71caebeaa5d8070a
Nolan Locke <nolan.s.locke@gmail.com>
2025-10-23 18:08:02 -0700

    fix climber position

--- src/main/java/frc/robot/subsystems/climber/ClimberPositions.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberPositions.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.climber;
 
 public class ClimberPositions {
     public static final double CLIMBED= -0.025;
-    public static final double DEPLOYING = -0.25;
+    public static final double DEPLOYING = 0.25;
     public static final double IDLE = 0;
     public static final double CLIMBING = 0;
 }
