d5f8b098be15341b7ebfc757e1aa4eca60c5f89e
Nolan Locke <nolan.s.locke@gmail.com>
2025-10-28 21:48:21 -0700

    maximize algae net height

--- src/main/java/frc/robot/subsystems/armManager/elevator/ElevatorState.java
+++ src/main/java/frc/robot/subsystems/armManager/elevator/ElevatorState.java
@@ -10,7 +10,7 @@ public enum ElevatorState {
     HIGH_REEF_ALGAE(0.9),
     LOW_REEF_ALGAE(0.53),
     GROUND_ALGAE(0.16),
-    ALGAE_NET(1.38 + Units.inchesToMeters(0.75)),
+    ALGAE_NET(1.38 + Units.inchesToMeters(1.5)),
     ALGAE_PROCESSOR(0.1),
     HANDOFF(0.785 - Units.inchesToMeters(0.75)), // Actual handoff position
     IDLE_EMPTY(0.825), // State above the intake that allows the intake to move in/out freely
