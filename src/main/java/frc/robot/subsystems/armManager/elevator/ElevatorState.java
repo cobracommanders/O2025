589e88f852b7f7ace0b937cd90ad1f9ae28c0023
Nolan Locke <nolan.s.locke@gmail.com>
2025-10-29 19:37:02 -0700

    change scoring and intaking positions

--- src/main/java/frc/robot/subsystems/armManager/elevator/ElevatorState.java
+++ src/main/java/frc/robot/subsystems/armManager/elevator/ElevatorState.java
@@ -15,10 +15,10 @@ public enum ElevatorState {
     HANDOFF(0.785 - Units.inchesToMeters(0.75)), // Actual handoff position
     IDLE_EMPTY(0.825), // State above the intake that allows the intake to move in/out freely
     LOLLIPOP(0),
-    PREPARE_L4(1.3),
+    PREPARE_L4(1.418),
     PREPARE_L3(0.765),
     PREPARE_L2(0.35),
-    SCORE_L4(1.2),
+    SCORE_L4(1.25),
     SCORE_L3(0.634),
     SCORE_L2(0.219),
     CUSTOM(0.0); // Set in the elevator class
--- src/main/java/frc/robot/subsystems/ground_manager/intake/IntakePivotPositions.java
+++ src/main/java/frc/robot/subsystems/ground_manager/intake/IntakePivotPositions.java
@@ -3,6 +3,6 @@ public class IntakePivotPositions {
     public static double IDLE = 0.318;
     public static double INTAKING = -0.036;
     public static double HANDOFF = 0.318;
-    public static double SCORE_L1 = 0.25;
+    public static double SCORE_L1 = 0.23;
     public static double CLIMB = 0.227;
 }
