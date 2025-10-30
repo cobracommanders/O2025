00630c0bd45788c15a7b4a731af51ae48a3289d2
GavinDistel <gdistel@498robotics.org>
2025-10-29 20:06:41 -0700

    Lowered L1 speed

--- src/main/java/frc/robot/subsystems/ground_manager/intakeRollers/IntakeRollersSpeeds.java
+++ src/main/java/frc/robot/subsystems/ground_manager/intakeRollers/IntakeRollersSpeeds.java
@@ -4,6 +4,6 @@ public class IntakeRollersSpeeds {
     public static final double IDLE = 0.0;
     public static final double INTAKING = -1;
     public static final double HANDOFF = 1.0;
-    public static final double SCORE_L1 = 0.7;
+    public static final double SCORE_L1 = 0.55;
     public static final double CLIMB = 0.0;
 }
