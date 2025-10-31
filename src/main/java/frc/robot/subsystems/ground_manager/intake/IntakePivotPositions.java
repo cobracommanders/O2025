46ec3437c54e85f1fa6c00851c5a7c48d9a9dc54
Nolan Locke <nolan.s.locke@gmail.com>
2025-10-30 23:18:08 -0700

    lower intaking position by 1.5 "ticks"

--- src/main/java/frc/robot/subsystems/ground_manager/intake/IntakePivotPositions.java
+++ src/main/java/frc/robot/subsystems/ground_manager/intake/IntakePivotPositions.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems.ground_manager.intake;
 public class IntakePivotPositions {
     public static double IDLE = 0.318;
-    public static double INTAKING = -0.036;
+    public static double INTAKING = -0.036 - .004 - .002;
     public static double HANDOFF = 0.318;
     public static double SCORE_L1 = 0.23;
     public static double CLIMB = 0.227;
